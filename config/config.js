cdb1c52b6db764ff9ad0c0ad8c7b98eb7d60bde1
Alejocram <alejocram@gmail.com>
2017-10-31 18:20:26 -0500

    Initial commit with yeoman template and adding bootstrap support.

---
+++ config/config.js
@@ -0,0 +1,37 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'gutenberg-web'
+    },
+    port: process.env.PORT || 3000,
+    db: 'sqlite://localhost/gutenberg-web-development',
+    storage: rootPath + '/data/gutenberg-web-development'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'gutenberg-web'
+    },
+    port: process.env.PORT || 3000,
+    db: 'sqlite://localhost/gutenberg-web-test',
+    storage: rootPath + '/data/gutenberg-web-test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'gutenberg-web'
+    },
+    port: process.env.PORT || 3000,
+    db: 'sqlite://localhost/gutenberg-web-production',
+    storage: rootPath + 'data/gutenberg-web-production'
+  }
+};
+
+module.exports = config[env];
